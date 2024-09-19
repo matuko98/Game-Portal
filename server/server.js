e8188f7a39ed21b52252a9247ba4b60616ab4532
matuko98 <danielmatuko19809@gmail.com>
2024-09-19 16:26:47 +0300

    start project build

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = 3000;
+
+const app = express();
+
+// controlles פונקציות של הבאקנד
+// router נתיבים של הראוטר
